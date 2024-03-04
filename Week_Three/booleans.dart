27866b58acdcc72cc067e3017dce2951fb7ca779
MBEKE <ogadambeke@gmail.com>
2024-03-04 23:19:52 +0300

    Add: booleans.dart file
    The program demonstrates the declaration and use of Booleans in Dart

---
+++ Week_Three/booleans.dart
@@ -0,0 +1,13 @@
+// The keyword 'bool' is used to represent a Boolean literal
+
+//main - entry point
+void main(){
+  /* Declaration of Boolean variable
+  bool var_name = true;
+  OR
+  bool var_name = false;
+  */
+  bool test;
+  test = 12 > 5;
+  print(test);
+}
