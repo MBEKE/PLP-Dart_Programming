4ea68d281f0500715e3acef6ec6fe8c6e3ed503f
MBEKE <ogadambeke@gmail.com>
2024-04-05 14:46:54 +0300

    demo: the constuctor in dart

---
+++ Week_5/defaultConstructor.dart
@@ -0,0 +1,11 @@
+class MyDetails {
+  // Default Constructor.
+  MyDetails() {
+    // Initialize code if needed
+  }
+}
+
+void main() {
+  // Creating an instance of MyDetails using default constructor
+  var myDetails = MyDetails();
+}
